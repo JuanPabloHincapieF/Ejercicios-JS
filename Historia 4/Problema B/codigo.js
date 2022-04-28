d2a93f94656ada9bcdf3a28c9a1098a1b16bc1cb
Juan Pablo Hincapie F <jphf0527@gmail.com>
2022-04-28 09:57:07 -0500

    Resolviendo problema 4A

---
+++ Historia 4/Problema B/codigo.js
@@ -0,0 +1,8 @@
+/* La facultad de Cofla está por comenzar y las 12 materias de la carrera tienen asignado un profesor y todos los alumnos que se anotaron en dichas clases, pero necesitamos ver esto mas ordenadamente.
+
+        - Crear una funcion que al pasarle como parametro la materia nos devuelva: 
+            * el profesor asignado 
+            * el nombre de todos los alumnos 
+        - Crear una funcion que nos diga en cuantas clases esta cofla
+        - Nombrar las clases en las que esta y los profesores de cada una.
+*/
